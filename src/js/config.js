25e6c77a6e3d752ba9e26f570cba206df014e336
peeta98 <pedromnmoreira98@gmail.com>
2023-10-15 13:10:40 +0000

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '5ff89ea4-a60d-41b2-91c5-5df6bddba35b';
+export const MODAL_CLOSE_SEC = 2.5;
